55055a7c97ec1b5a69965c2932a2d0926d5be683
42CrMo4 <44754810+42CrMo4@users.noreply.github.com>
2020-06-17 21:25:03 +0200

    format and comment source code button

--- software/button/button.ino
+++ software/button/button.ino
@@ -1,7 +1,11 @@
-/*    Arduino Long Range Wireless Communication using HC-12
-                      Example 01
-   by Dejan Nedelkovski, www.HowToMechatronics.com
+/*  OnAir-Sign button software
+    https://github.com/42CrMo4/OnAir-Sign
+    
+    based on :
+      * Arduino Long Range Wireless Communication using HC-12: Example 01 by Dejan Nedelkovski, www.HowToMechatronics.com
+      * FastLED
 */
+
 #include <SoftwareSerial.h>
 #include <FastLED.h>
 
@@ -10,86 +14,88 @@ SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
 // How many leds in your strip?
 #define NUM_LEDS 12
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Data Pin for the LED Strip
 #define DATA_PIN 3
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
+// Button Pin Cherry MX switch
 const int b = 2;
 
+// variables
 int state = 0;
-//int art = 0;
 byte incomingByte;
 String readBuffer = "";
 
 void setup() {
-  Serial.begin(9600);             // Serial port to computer
-  HC12.begin(9600);// Serial port to HC12
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
-  pinMode(b, INPUT);
-  on_START();
+  HC12.begin(9600);               // Serial port to HC12
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // initialize the LED strip
+  pinMode(b, INPUT);              // initialize button als input
+  on_START();                     // run start sequent of LEDs
 }
 void loop() {
-  while (HC12.available()) {        // If HC-12 has data
-    incomingByte = HC12.read();          // Store each icoming byte from HC-12
-    readBuffer += char(incomingByte);    // Add each byte to ReadBuffer string variable
+  // register messages from sign
+  while (HC12.available()) {              // If HC-12 has data
+    incomingByte = HC12.read();           // Store each icoming byte from HC-12
+    readBuffer += char(incomingByte);     // Add each byte to ReadBuffer string variable
   }
-  while (Serial.available()) {      // If Serial monitor has data
-    HC12.write(Serial.read());      // Send that data to HC-12
-  }
+  
+  // register button press
   if (digitalRead (b) == HIGH) {
     if (state == 0) {
-      HC12.write("1");
-      state = 1;
+      HC12.write("1");    // transmit on to sign
+      state = 1;          // remenber button state
     }
     else if (state == 1 ) {
-      HC12.write("2");
-      state = 0;
+      HC12.write("2");    // transmit off to sign
+      state = 0;          // remenber button state
     }
     delay(800);
-//    art = 1;
   }
-    int i = readBuffer.toInt();
-  if (i == 1){
+  
+  // convert string of HC-12 buffer to an integer
+  int i = readBuffer.toInt();
+  
+  // if a signal has arrived decide which state 
+  if (i == 1){          // message on from sign 
     on_LED();
-    readBuffer = "";
+    readBuffer = "";    // reset readbuffer 
   }
-  if (i > 1){
+  if (i > 1){           // message off from sign
     off();
-    readBuffer = "";
+    readBuffer = "";    // reset readbuffer 
   }
+  
+  // reset readbuffer 
   readBuffer = "";
 }
 
 void on_START() {
-  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
-  FastLED.show();
+  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));      // set all LEDS to full brightness
+  FastLED.show();                                   // display LED settings
   delay(500);
   // Now turn the LED off, then pause
-  FastLED.clear();
-  FastLED.show();
+  FastLED.clear();                                  // clear all LEDs
+  FastLED.show();                                   // display LED settings
 
-     for(int n = 0; n < NUM_LEDS; n = n + 1) {
-      leds[n] = CRGB(255, 0, 0);
-      leds[n-1] = CRGB(150, 0, 0);
-      leds[n-2] = CRGB(0, 0, 0);
-//      leds[n].maximizeBrightness();
-      FastLED.show();
+  for(int n = 0; n < NUM_LEDS; n = n + 1) {         // cicle trough all LED of the strip
+      leds[n] = CRGB(255, 0, 0);                    // set the first LED to full
+      leds[n-1] = CRGB(150, 0, 0);                  // reduce brightness of the previous LED
+      leds[n-2] = CRGB(0, 0, 0);                    // turn off the previous of teh previous LED
+      FastLED.show();                               // display LED settings
       FastLED.delay(100);
-     }
-  FastLED.clear();
-  FastLED.show();
+  }
+  FastLED.clear();                                  // clear all LEDs
+  FastLED.show();                                   // display LED settings
 }
 
 void on_LED() {
-  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
-  FastLED.show();
+  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));      // set all LEDS to full brightness  
+  FastLED.show();                                   // display LED settings
 }
 
 void off() {
-  FastLED.clear();
-  FastLED.show();
+  FastLED.clear();                                  // clear all LEDs
+  FastLED.show();                                   // display LED settings
 }
