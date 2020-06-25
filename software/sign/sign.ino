bd39f71f573a8d9086158e3668f1e799d3aee70d
42CrMo4 <44754810+42CrMo4@users.noreply.github.com>
2020-06-25 10:42:02 +0200

    format and comment source code sign

--- software/sign/sign.ino
+++ software/sign/sign.ino
@@ -1,67 +1,74 @@
-/*    Arduino Long Range Wireless Communication using HC-12
-                      Example 01
-   by Dejan Nedelkovski, www.HowToMechatronics.com
+/*  OnAir-Sign sign software
+    https://github.com/42CrMo4/OnAir-Sign
+
+    based on :
+        Arduino Long Range Wireless Communication using HC-12: Example 01 by Dejan Nedelkovski, www.HowToMechatronics.com
+        FastLED
 */
+
 #include <SoftwareSerial.h>
 #include <FastLED.h>
 
-SoftwareSerial HC12(10, 11); // HC-12 TX Pin, HC-12 RX Pin
+SoftwareSerial HC12(10, 11);                                 // HC-12 TX Pin, HC-12 RX Pin
 
 // How many leds in your strip?
 #define NUM_LEDS 20
 
-// For led chips like Neopixels, which have a data line, ground, and power, you just
-// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
-// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+// Data Pin for the LED Strip
 #define DATA_PIN 3
 
 // Define the array of leds
 CRGB leds[NUM_LEDS];
+
+// variables
 byte incomingByte;
 String readBuffer = "";
 
 void setup() {
-  Serial.begin(9600);             // Serial port to computer
-  HC12.begin(9600);               // Serial port to HC12
-  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);
-  on_START();
+  HC12.begin(9600);                                         // Serial port to HC12
+  FastLED.addLeds<WS2812B, DATA_PIN, RGB>(leds, NUM_LEDS);  // initialize the LED strip
+  on_START();                                               // run start sequent of LEDs
 }
+
 void loop() {
-  while (HC12.available()) {             // If HC-12 has data
-    incomingByte = HC12.read();          // Store each icoming byte from HC-12
-    readBuffer += char(incomingByte);    // Add each byte to ReadBuffer string variable
+  while (HC12.available()) {                                // If HC-12 has data
+    incomingByte = HC12.read();                             // Store each icoming byte from HC-12
+    readBuffer += char(incomingByte);                       // Add each byte to ReadBuffer string variable
   }
+
   delay(100);
-  Serial.print(readBuffer);
-  int i = readBuffer.toInt();
-  if (i == 1){
+
+  int i = readBuffer.toInt();                               // convert string of HC-12 buffer to an integer
+
+  // if a signal has arrived decide which state
+  if (i == 1) {                                             // message on from button
     on_LED();
-    readBuffer = "";
+    readBuffer = "";                                        // reset readbuffer
   }
-  if (i > 1){
+  if (i > 1) {                                              // message off from button
     off();
-    readBuffer = "";
+    readBuffer = "";                                        // reset readbuffer
   }
-  readBuffer = "";
+  readBuffer = "";                                          // reset readbuffer
 }
 
 void on_START() {
-  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
-  FastLED.show();
+  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));              // set all LEDS to full brightness
+  FastLED.show();                                           // display LED settings
   delay(500);
   // Now turn the LED off, then pause
-  FastLED.clear();
-  FastLED.show();
+  FastLED.clear();                                          // clear all LEDs
+  FastLED.show();                                           // display LED settings
 }
 
 void on_LED() {
-  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));
-  FastLED.show();
-  HC12.write("1");
+  fill_solid(leds, NUM_LEDS, CRGB(255, 0, 0));              // set all LEDS to full brightness
+  FastLED.show();                                           // display LED settings
+  HC12.write("1");                                          // transmit on to button
 }
 
 void off() {
-  FastLED.clear();
-  FastLED.show();
-  HC12.write("2");
+  FastLED.clear();                                          // clear all LEDs
+  FastLED.show();                                           // display LED settings
+  HC12.write("2");                                          // transmit off to button
 }
